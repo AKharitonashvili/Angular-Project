c10dcce5b0fd5d8be31bf46070623fb7f637c7a6
sandro sandro <sandroxari@gmail.com>
2023-01-11 23:13:36 +0400

    feature: loader added

--- src/app/applications/applications.module.ts
+++ src/app/applications/applications.module.ts
@@ -4,12 +4,13 @@ import { ApplicationsComponent } from './applications.component';
 import { ApplicationsRestService } from './services/rest/applications-rest.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AccountComponent } from './components/account/account.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 const components = [ApplicationsComponent, AccountComponent];
 const services = [ApplicationsRestService];
 
 @NgModule({
-  imports: [CommonModule, MatProgressSpinnerModule],
+  imports: [CommonModule, MatProgressSpinnerModule, MatProgressBarModule],
   exports: [...components],
   declarations: [...components],
   entryComponents: [...components],
