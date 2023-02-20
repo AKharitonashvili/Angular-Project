8a085b51913f67dbffe953d6c215993e09a5c0c2
sandro sandro <sandroxari@gmail.com>
2023-02-20 22:46:48 +0400

    feature: style transaction history

--- src/app/transactions/components/transaction-history-header/transaction-history-header.component.ts
+++ src/app/transactions/components/transaction-history-header/transaction-history-header.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-transaction-history-header',
@@ -6,8 +6,4 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./transaction-history-header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TransactionHistoryHeaderComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class TransactionHistoryHeaderComponent {}
